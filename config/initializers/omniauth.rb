acf8050323be1f354a8d6f29bdd8c8e1bcb98856
John Davison <johncdavison@gmail.com>
2013-01-09 21:04:09 -0800

    updated linkedin initialize details

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :twitter, ENV['RWB_KEY'], ENV['RWB_SECRET']
-  provider :linkedin, "bp273rnw3bga", "FUzm8Mump9xPBvtz"
+  provider :linkedin, ENV['RWB_LI_KEY'], ENV['RWB_LI_SECRET'], :scope => 'r_fullprofile r_emailaddress r_network', :fields => ["id", "email-address", "first-name", "last-name", "headline", "industry", "picture-url", "public-profile-url", "location", "connections"]
   # provider :linkedin, '6uc3tiqssjji', 'YcuMHrzaW93m8b4L'
   
 end
