f390839c1b19287a1a4d917e2f94c8de170e9e27
John Davison <johncdavison@gmail.com>
2013-01-23 22:43:59 -0800

    showing user rsvp list

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,7 @@
 module EventsHelper
+  def has_rsvps?(event_id)
+    if Rsvp.where("event_id = ?", event_id)
+      true
+    end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ActiveRecord::Base
   attr_accessible :capacity, :date, :location, :title, :description, :start_time, :end_time
 
+  def all_users_who_rsvp
+    @users_who_rsvp = []
+    Rsvp.where("event_id = ?", self.id).each do |rsvp|
+      @users_who_rsvp << User.find(rsvp.user_id)
+    end
+    @users_who_rsvp
+  end
+
 end
